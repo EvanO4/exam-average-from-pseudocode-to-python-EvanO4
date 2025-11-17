c5ef8393b7102c8e2ca7c2c435b40455c43d90e5
IS250 Student <changeme@example.com>
2025-11-17 15:04:55 -0600

    Created psudocode

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 """
-Full Name:
-Class-Section:
-Assignment Title:
-Submission Date:
-"""
-"""
-Write your pseudocode here.
-Do not write any Python code in this section.
-Your pseudocode should describe your overall approach in your own words.
+Full Name:  Evan O'Hara
+Class-Section: 1
+Assignment Title: average from pseudocode to python
+Submission Date: 11/17/25
 """
 
-# Your Python code begins below this line.
-# Every line you write must have a comment directly above it.
 
-# Call your function when your program is ready
+#creates function to calculate exam scores
+
+#has user input the first test score
+
+#has user input the second test score
+
+#has user input the third test score
+
+#add's up the 3 test scores
+
+#prints the first exam score
+
+#prints the second exam score
+
+#prints the third exam score
+
+#gets the average of the 3 exam scores
+
+
+#calls function back to main and runs it
 calculate_average()   
   
