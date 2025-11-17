0636db57d8115f36f41b7e7cefec4615abe1b744
IS250 Student <changeme@example.com>
2025-11-17 15:09:08 -0600

    Finished code that calculates exam average

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ Submission Date: 11/17/25
 
 
 #creates function to calculate exam scores
-
-#has user input the first test score
-
-#has user input the second test score
-
-#has user input the third test score
-
-#add's up the 3 test scores
-
-#prints the first exam score
-
-#prints the second exam score
-
-#prints the third exam score
-
-#gets the average of the 3 exam scores
-
+def calculate_average():
+    #has user input the first test score
+    score1 = float(input('Enter your first score: '))
+    #has user input the second test score
+    score2 = float(input('Enter your second score: '))
+    #has user input the third test score
+    score3 = float(input('Enter your thrid score: '))
+    #add's up the 3 test scores
+    added_score = score1 + score2 + score3
+    #prints the first exam score
+    print(f'your first exam score was: {score1}')
+    #prints the second exam score
+    print(f'your second exam score was: {score2}')
+    #prints the third exam score
+    print(f'your third exam score was: {score3}')
+    #gets the average of the 3 exam scores
+    print(added_score / 3)
 
 #calls function back to main and runs it
 calculate_average()   
